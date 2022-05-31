d988d1603ec0c54b6369cfb71f31f3390d8f453f
Angel Newton <xanewton@gmail.com>
2022-05-31 15:03:50 -0400

    Initial Commit

---
+++ SwiftfulThinkingTodoList/SwiftfulThinkingTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingTodoListApp.swift
+//  SwiftfulThinkingTodoList
+//
+//  Created by newtan on 2022-05-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
